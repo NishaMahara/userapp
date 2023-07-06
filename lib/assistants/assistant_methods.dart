1c23c03473f72fc17c65ffa6862ab1d2494ab454
Nisha Mahara <nishamahara86@gmail.com>
2023-07-06 12:55:51 +0545

    map assistant work

--- lib/assistants/assistant_methods.dart
+++ lib/assistants/assistant_methods.dart
@@ -1,8 +1,19 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:shringar1_app/assistants/request_assistant.dart';
 import 'package:shringar1_app/global/global.dart';
+import 'package:shringar1_app/global/map_key.dart';
 import 'package:shringar1_app/models/user_model.dart';
 
-class AssistantMethods {
+class AssistantMethods
+{
+  static Future<String> searchAddressForGeographicCoordinates(Position position) async
+  {
+  await RequestAssistant.receiveRequest(apiUrl);
+  {
+    String apiUrl = "https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$mapKey";
+  }
   static void readCurrentOnlineUserInfo() async
   {
     currentFirebaseUser = fAuth.currentUser;
